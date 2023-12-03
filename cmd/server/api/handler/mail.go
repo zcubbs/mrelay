91eb590229de4b9eb9e7a2f1877756ee501a1f5b
zcubbs <zackcubby@gmail.com>
2023-12-03 02:04:34 +0100

    :green_heart: update ci

--- cmd/server/api/handler/mail.go
+++ cmd/server/api/handler/mail.go
@@ -1,35 +1,35 @@
 package handler
 
 import (
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/render"
-    "github.com/zcubbs/mrelay/cmd/server/auth"
-    "github.com/zcubbs/mrelay/cmd/server/db"
-    "github.com/zcubbs/mrelay/cmd/server/mail"
-    "net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/zcubbs/mrelay/cmd/server/auth"
+	"github.com/zcubbs/mrelay/cmd/server/db"
+	"github.com/zcubbs/mrelay/cmd/server/mail"
+	"net/http"
 )
 
 type MailHandler struct {
-    MailStore db.MailStore
-    Mailer    mail.Mailer
+	MailStore db.MailStore
+	Mailer    mail.Mailer
 }
 
 func NewMailHandler(
-    mailStore db.MailStore,
-    mailer mail.Mailer,
+	mailStore db.MailStore,
+	mailer mail.Mailer,
 ) *MailHandler {
-    return &MailHandler{
-        MailStore: mailStore,
-        Mailer:    mailer,
-    }
+	return &MailHandler{
+		MailStore: mailStore,
+		Mailer:    mailer,
+	}
 }
 
 func (h *MailHandler) Routes(realm string, store db.AuthStore) chi.Router {
-    r := chi.NewRouter()
-    r.With(auth.BasicAuth(realm, store)).Post("/smtp", h.handleSendSmtpMail)
-    return r
+	r := chi.NewRouter()
+	r.With(auth.BasicAuth(realm, store)).Post("/smtp", h.handleSendSmtpMail)
+	return r
 }
 
 func (h *MailHandler) handleSendSmtpMail(w http.ResponseWriter, r *http.Request) {
-    render.JSON(w, r, "OK")
+	render.JSON(w, r, "OK")
 }
