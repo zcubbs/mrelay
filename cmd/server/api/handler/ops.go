2ee11a47e4d496fe3d0ae400d8603a1f36f730c7
zcubbs <zackcubby@gmail.com>
2023-12-03 02:58:00 +0100

    :beers: add swagger

--- cmd/server/api/handler/ops.go
+++ cmd/server/api/handler/ops.go
@@ -1,30 +1,30 @@
 package handler
 
 import (
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/render"
-    "net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"net/http"
 )
 
 type OpsHandler struct {
-    Version string
-    Commit  string
-    Date    string
+	Version string
+	Commit  string
+	Date    string
 }
 
 func NewOpsHandler(version, commit, date string) *OpsHandler {
-    return &OpsHandler{
-        Version: version,
-        Commit:  commit,
-        Date:    date,
-    }
+	return &OpsHandler{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+	}
 }
 
 func (h *OpsHandler) Routes() chi.Router {
-    r := chi.NewRouter()
-    r.Get("/health", h.Health)
-    r.Get("/version", h.VersionInfo)
-    return r
+	r := chi.NewRouter()
+	r.Get("/health", h.Health)
+	r.Get("/version", h.VersionInfo)
+	return r
 }
 
 // Health - Returns a simple health check.
@@ -36,13 +36,13 @@ func (h *OpsHandler) Routes() chi.Router {
 // @Success 200 {string} response "api response"
 // @Router / [get]
 func (h *OpsHandler) Health(w http.ResponseWriter, r *http.Request) {
-    render.JSON(w, r, map[string]string{"status": "ok"})
+	render.JSON(w, r, map[string]string{"status": "ok"})
 }
 
 func (h *OpsHandler) VersionInfo(w http.ResponseWriter, r *http.Request) {
-    render.JSON(w, r, map[string]string{
-        "version": h.Version,
-        "commit":  h.Commit,
-        "date":    h.Date,
-    })
+	render.JSON(w, r, map[string]string{
+		"version": h.Version,
+		"commit":  h.Commit,
+		"date":    h.Date,
+	})
 }
