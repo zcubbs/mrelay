f6917a0dd31f9258347259346a8736e14008f7e6
rEnergYr <elric.gumbau@icloud.com>
2023-12-18 16:23:15 +0100

    :lipstick: Setup tailwindcss & shadcn-vue

--- web/dashboard/nuxt.config.ts
+++ web/dashboard/nuxt.config.ts
@@ -1,3 +1,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component, eg: shad-button
+     */
+
+    prefix: 'Shad',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/shad"
+     */
+    componentDir: './components/shad',
+  },
 })
