12df8c174f213cd0e1c1f6a03f6b3b756630e293
rEnergYr <elric.gumbau@icloud.com>
2023-12-19 15:12:59 +0100

    :rotating_light: Add linter rules

--- web/dashboard/components/shad/button/Button.vue
+++ web/dashboard/components/shad/button/Button.vue
@@ -15,11 +15,7 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <Primitive
-    :as="as"
-    :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), $attrs.class ?? '')"
-  >
+  <Primitive :as="as" :as-child="asChild" :class="cn(buttonVariants({ variant, size }), $attrs.class ?? '')">
     <slot />
   </Primitive>
 </template>
